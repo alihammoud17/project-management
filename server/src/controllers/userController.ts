70cddad94312a6e671ebf1a61475dd54692d50f0
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-10-19 19:01:31 +0300

    add create user route

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -11,3 +11,30 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
         res.status(500).json({ message: "Error retreiving users " + error.message });
     }
 }
+
+export const createUser = async (req: Request, res: Response) => {
+
+    try {
+
+        const {
+            username,
+            cognitoId,
+            profilePictureUrl = 'i1.jpg',
+            teamId = 1
+        } = req.body;
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                cognitoId,
+                profilePictureUrl,
+                teamId
+            }
+        });
+        res.json({ message: "New user created", newUser });
+    } catch (error: any) {
+        res.status(500).json({ message: "Error creating user " + error.message });
+    }
+
+
+}
